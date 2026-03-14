086d719049732c05a9febd0ad1e5f81701e3d95f
Your Name <you@example.com>
2026-03-14 13:02:43 +0530

    test

--- loans/tests.py
+++ loans/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Customer
 
-# Create your tests here.
+class CustomerTest(TestCase):
+
+    def test_customer_creation(self):
+
+        customer = Customer.objects.create(
+            first_name="Rahul",
+            last_name="Sharma",
+            phone_number="9999999999",
+            monthly_salary=50000,
+            approved_limit=1800000,
+            current_debt=0
+        )
+
+        self.assertEqual(customer.first_name, "Rahul")
